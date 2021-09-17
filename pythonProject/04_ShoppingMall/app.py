0d76d9ef16a930ab4a3dbcf5af72fd92d3d9cf4c
thalals <thalsal@naver.com>
2021-09-17 09:30:25 +0900

    connect AWS-mongoDB with ID, PW

--- pythonProject/04_ShoppingMall/app.py
+++ pythonProject/04_ShoppingMall/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+#pymongo 계정 접속 id:passwprd
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 
